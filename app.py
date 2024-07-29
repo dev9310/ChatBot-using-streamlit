c5f3cbf56b41e2ad7c4b3739cf2cc25c287c32fd
Dev Shakeywar <122546955+bannu82@users.noreply.github.com>
2024-07-29 11:14:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,68 +6,79 @@ import time
 if "key" not in st.session_state:
     st.session_state.key = 'AIzaSyCi2c-H5RHN-OO4XOa3lCCcqsL_lxT6fNg'
 
-
 genai.configure(api_key=st.session_state.key)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
+# Title
+st.markdown('<h1 >Chat Bot</h1>',unsafe_allow_html=True)
+
+# Element Css
 st.markdown(helper.get_css() , unsafe_allow_html=True)
 
+# SideBar
 with st.sidebar:
     st.markdown("<h1>Welcome</h1>",unsafe_allow_html=True)
     st.divider()
-topic_list = []
+    container = st.container(height=300)
 
+
+
+# Stream Data
 def stream_data(txt ,sleep_time=0.05):
     for word in txt.split(" "):
         yield word + " "
         time.sleep(sleep_time)
 
 
-def create_recent_topic(topic):
-    with st.sidebar:
-        
-        topic_list.append(topic)
-
-        st.markdown("<h2>Recents</h2>",unsafe_allow_html=True)
-        container = st.container(height=300)
-        for i in topic_list:
-            
-            st.sidebar.text(i)
-            container.chat_message("user").write_stream(stream_data(i,0.6))
+# # Topic session state
+# if "topic" not in st.session_state:
+#     st.session_state.topic =[]
 
 
+# # Save history data
+# for topic in st.session_state.topic:
+#     with container.chat_message(topic["role"]):
+#         st.markdown(topic["message"])
 
-def create_user_message(prompt):
+if "message" not in st.session_state:
+    st.session_state.message =[]
 
-    ...
+# Save history data
+for msg in st.session_state.message:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["message"])
 
-def create_bot_message(res):
-    ...
+
+
+
 
 
 def intro():
     st.markdown(helper.get_intro())
 
-def main():
 
+def main():
     
-    st.markdown('<h1 >Chat Bot</h1>',unsafe_allow_html=True)
     
     messages = st.container()
     prompt = st.chat_input("Say something")
 
     if prompt:      
-
-        create_user_message(prompt)
-        
-
         messages.chat_message("human").write(prompt)
         response = model.generate_content(prompt)
         res = helper.to_markdown(response.text)
-        create_bot_message(res)
-
+        
         st.chat_message("ai").write_stream(stream_data(res))
-        create_recent_topic(prompt)
+
+        # Added messages to history
+        st.session_state.message.append({"role":"user" ,"message":prompt})
+        st.session_state.message.append({"role":"ai" ,"message":res})
+
+        # with st.sidebar :
+        #     container.chat_message("User").write_stream(stream_data(prompt,0.6))
+        # st.session_state.topic.append({"role":"human" ,"message":prompt })  
+
 
 
 main()
+
